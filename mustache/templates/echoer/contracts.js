877027e7e0a087db749f9ed536301d2601f399c9
Topdefi <94203269+topdefi@users.noreply.github.com>
2022-11-15 09:21:23 -0800

    Updates

--- mustache/templates/echoer/contracts.js
+++ mustache/templates/echoer/contracts.js
@@ -1,21 +1,9 @@
 const contracts = [
   {
-    prod: 22927201,
+    prod: 8159269,
     network: 'bsc',
     name: 'Echoer',
-    address: '0x25005bab484d357d5117574f3e6Aafa4B47c1b2B',
-  },
-  {
-    prod: 17754564,
-    network: 'xdai',
-    name: 'Echoer',
-    address: '0x25005bab484d357d5117574f3e6Aafa4B47c1b2B',
-  },
-  {
-    prod: 16257996,
-    network: 'matic',
-    name: 'Echoer',
-    address: '0x25005bab484d357d5117574f3e6Aafa4B47c1b2B',
+    address: '0xa75BF2815618872f155b7C4B0C81bF990f5245E4',
   },
   {
     prod: 11842486,
@@ -24,28 +12,10 @@ const contracts = [
     address: '0x9B27DD5Bb15d42DC224FCD0B7caEbBe16161Df42',
   },
   {
-    prod: 3781595,
-    network: 'goerli',
+    prod: 28845148,
+    network: 'mumbai',
     name: 'Echoer',
-    address: '0x37e6859804b6499d1e4a86d70a5fdd5de6a0ac65',
-  },
-  {
-    prod: 4429813,
-    network: 'avalanche',
-    name: 'Echoer',
-    address: '0x25005bab484d357d5117574f3e6Aafa4B47c1b2B',
-  },
-  {
-    prod: 3430605,
-    network: 'arbitrum-one',
-    name: 'Echoer',
-    address: '0x25005bab484d357d5117574f3e6Aafa4B47c1b2B',
-  },
-  {
-    prod: 2243694,
-    network: 'optimism',
-    name: 'Echoer',
-    address: '0x25005bab484d357d5117574f3e6Aafa4B47c1b2B',
+    address: '0x4AFFFD0F9cD435A01115b3258D88c3777cAcB104',
   },
 ];
 
--- mustache/templates/proxy/contracts.js
+++ mustache/templates/proxy/contracts.js
@@ -1,51 +1,15 @@
 const contracts = [
   {
-    prod: 22927490,
+    prod: 22554197,
     name: 'Proxy',
     network: 'bsc',
-    address: '0x908f155Df533AF20c4ED32af691B327eCc35e449',
+    address: '0xbd89f03565025aAE6d6541c37A8B17EFf0E79887',
   },
   {
-    prod: 17754561,
-    name: 'Proxy',
-    network: 'xdai',
-    address: '0x908f155Df533AF20c4ED32af691B327eCc35e449',
-  },
-  {
-    prod: 16257989,
-    name: 'Proxy',
-    network: 'matic',
-    address: '0x908f155Df533AF20c4ED32af691B327eCc35e449',
-  },
-  {
-    prod: 14248730,
+    prod: 15843570,
     name: 'Proxy',
     network: 'mainnet',
-    address: '0xd90e2f925DA726b50C4Ed8D0Fb90Ad053324F31b',
-  },
-  {
-    prod: 3781595,
-    name: 'Proxy',
-    network: 'goerli',
-    address: '0x454d870a72e29d5e5697f635128d18077bd04c60',
-  },
-  {
-    prod: 4429818,
-    name: 'Proxy',
-    network: 'avalanche',
-    address: '0x908f155Df533AF20c4ED32af691B327eCc35e449',
-  },
-  {
-    prod: 3430648,
-    name: 'Proxy',
-    network: 'arbitrum-one',
-    address: '0x908f155Df533AF20c4ED32af691B327eCc35e449',
-  },
-  {
-    prod: 2243689,
-    name: 'Proxy',
-    network: 'optimism',
-    address: '0x908f155Df533AF20c4ED32af691B327eCc35e449',
+    address: '0xbd89f03565025aAE6d6541c37A8B17EFf0E79887',
   },
 ];
 
